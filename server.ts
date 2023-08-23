531230cd4ec9c302c388fe26a73583557a911e6d
Julio Colon <julio.colon@microsoft.com>
2023-08-23 02:30:12 +0000

    clean up

--- server.ts
+++ server.ts
@@ -5,56 +5,40 @@ const { exec, spawn } = require('child_process');
 const app = express();
 const port = 3000;
 
+let console_output = '';
+
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-app.get('/script', (req, res) => {
+app.post('/script', (req, res) => {
 
-  const bashScript = 'ls -la'; // Replace with your CLI script
-
-  // const childProcess = spawn('ls',['-la']);
   const childProcess = spawn('pwsh',['createRG.ps']);
 
   childProcess.stdout.on('data', (data) => {
     const output = data.toString();
-    console.debug(output);
-    res.write(`<pre>${output}</pre>`);
-    res.end();
+    
+    console.debug(output);    
+    console_output = console_output + `<pre>${output}</pre>`;
   });
 
   childProcess.stderr.on('data', (data) => {
     const errorOutput = data.toString();
-    res.write(`<pre style="color: red;">${errorOutput}</pre>`);
+    console.error(errorOutput);
+    console_output = console_output + `<pre style="color: red;">${errorOutput}</pre>`;
   });
 
   childProcess.on('close', (code) => {
-    res.write(`<pre>CLI process exited with code ${code}</pre>`);
-    res.end();
+    console.debug(code);
+    console_output = console_output + `<pre>CLI process exited with code ${code}</pre>`;
   });
-  // exec(bashScript, (error, stdout, stderr) => {
-  //   if (error) {
-  //     console.error('Error executing script:', error);
-  //     return res.send('An error occurred while executing the script.');
-  //   }
-
-  //   const output = stdout || stderr || 'No output';
-  //   res.send(`
-  //     <html>
-  //       <head>
-  //         <title>CLI Command Output</title>
-  //       </head>
-  //       <body>
-  //         <pre>${output}</pre>
-  //       </body>
-  //     </html>
-  //   `);
-  // });
-
+  
 });
 
-// app.get('/update', (req, res) => {
-//   res.
-// }
+// Serve the output of the console
+app.get('/console', (req, res) => {
+  res.write(console_output);
+  res.end();
+});
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
